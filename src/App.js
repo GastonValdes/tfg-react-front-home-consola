4912c567f853bf40f6e3a8959b20d248ac0cc34f
Gaston Raul Valdes <gastonrvaldes@hotmail.com>
2020-06-23 00:25:05 -0300

    resolvi la actualizacion en dinamica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import Pusher from 'pusher-js';
 
-//const API_URL = 'http://localhost:9000/api/';
-const API_URL = 'http://localhost:3010/';
 
+//const API_URL = 'http://localhost:9000/api/';
+const API_URL = 'http://localhost:3010/sensores';
 const PUSHER_APP_KEY = '9456bc135d545fdf5c87';
 const PUSHER_APP_CLUSTER = 'mt1';
+
 
 class App extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ class App extends Component {
     this.addTask = this.addTask.bind(this);
     this.removeTask = this.removeTask.bind(this);
   }
-    
+  
   updateText(e) {
     this.setState({ task: e.target.value });
   }
@@ -35,7 +35,9 @@ class App extends Component {
     const newTask = {
       task: this.state.task
     };
-    fetch(API_URL + 'new', {
+    alert('A form was submitted: ' + JSON.stringify(this.state));
+  //  fetch(API_URL + 'new', {
+    fetch(API_URL, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
@@ -51,12 +53,24 @@ class App extends Component {
   }
 
   addTask(newTask) {
+    fetch(API_URL)
+    .then (respuesta => respuesta.json() )
+    .then (resultado => {
+      this.setState({
+        isLoaded: true,
+        items: resultado,
+      })
+    })
+  }
+
+ /*
+  addTask(newTask) {
     this.setState(prevState => ({
       tasks: prevState.tasks.concat(newTask),
       task: ''
     }));
   }
-    
+  */  
   removeTask(id) {
     this.setState(prevState => ({
       tasks: prevState.tasks.filter(el => el.id !== id)
@@ -71,18 +85,45 @@ class App extends Component {
     this.channel = this.pusher.subscribe('sensores');
 	
     this.channel.bind('inserted', this.addTask);
-    this.channel.bind('deleted', this.removeTask);
-    this.channel.bind('updated', this.addTask);
-  }
-    
-  render() {
-    let tasks = this.state.tasks.map(item =>
-      <Task key={item.id} task={item} onTaskClick={this.deleteTask} />
-    );
+    this.channel.bind('deleted', this.addTask);
+       this.channel.bind('updated', this.addTask);
 
+      fetch(API_URL)
+         .then (respuesta => respuesta.json() )
+         .then (resultado => {
+           this.setState({
+             isLoaded: true,
+             items: resultado,
+           })
+         })
+
+  }
+  
+   
+  render() {
+  
+    let tasks = this.state.tasks.map(item =>
+    <Task key={item._id} task={item} onTaskClick={this.deleteTask} />
+    );
+    
+    var { isLoaded, items} = this.state;
+    if (!isLoaded){
+      return <div>Cargando...</div>
+    }
+    
     return (
+    
+      
       <div className="todo-wrapper">
         <form>
+          <h4>Sensores</h4>
+        <ul>
+          {items.map(item=>( 
+            <li key={item._id}>
+              {item.tipo} {item.subtipo} {item.medicion}              
+            </li>
+          ))}
+        </ul>
           <input type="text" className="input-todo" placeholder="New task" onChange={this.updateText} value={this.state.task} />
           <div className="btn btn-add" onClick={this.postTask}>+</div>
         </form>
