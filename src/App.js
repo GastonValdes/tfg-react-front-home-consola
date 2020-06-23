dc87cdd7912885b544b40dc983a9c7f462a0aa6d
Gaston Raul Valdes <gastonrvaldes@hotmail.com>
2020-06-23 20:08:24 -0300

    inclui los valores de configuracion y actuadores

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Pusher from 'pusher-js';
 
 
 //const API_URL = 'http://localhost:9000/api/';
-const API_URL = 'http://localhost:3010/sensores';
+const API_URL_SENSORES = 'http://localhost:3010/sensores';
+const API_URL_UMBRALES = 'http://localhost:3010/umbrales';
+const API_URL_ACTUADORES = 'http://localhost:3010/actuadores';
+
 const PUSHER_APP_KEY = '9456bc135d545fdf5c87';
 const PUSHER_APP_CLUSTER = 'mt1';
 
@@ -16,17 +19,19 @@ class App extends Component {
       tasks: [],
       task: ''
     };
-    this.updateText = this.updateText.bind(this);
-    this.postTask = this.postTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
+ //   this.updateText = this.updateText.bind(this);
+ //   this.postTask = this.postTask.bind(this);
+ //   this.deleteTask = this.deleteTask.bind(this);
     this.addTask = this.addTask.bind(this);
-    this.removeTask = this.removeTask.bind(this);
+ //   this.removeTask = this.removeTask.bind(this);
   }
-  
+
+/*
   updateText(e) {
     this.setState({ task: e.target.value });
   }
-
+*/
+ /* 
   postTask(e) {
     e.preventDefault();
     if (!this.state.task.length) {
@@ -37,7 +42,7 @@ class App extends Component {
     };
     alert('A form was submitted: ' + JSON.stringify(this.state));
   //  fetch(API_URL + 'new', {
-    fetch(API_URL, {
+    fetch(API_URL_SENSORES, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
@@ -45,22 +50,47 @@ class App extends Component {
       body: JSON.stringify(newTask)
     }).then(console.log);
   }
-    
+  */
+  
+ /* 
   deleteTask(id) {
-    fetch(API_URL + id, {
+    fetch(API_URL_SENSORES + id, {
       method: 'delete'
     }).then(console.log);
   }
-
+*/
   addTask(newTask) {
-    fetch(API_URL)
+
+    fetch(API_URL_SENSORES)
     .then (respuesta => respuesta.json() )
     .then (resultado => {
       this.setState({
-        isLoaded: true,
-        items: resultado,
-      })
+        isLoaded1: true,
+        items1: resultado,
+        })
+        console.log(resultado);
     })
+
+    fetch(API_URL_UMBRALES)
+    .then (respuesta => respuesta.json() )
+    .then (resultado => {
+      this.setState({
+        isLoaded2: true,
+        items2: resultado,
+      })
+      console.log(resultado);
+    })
+
+    fetch(API_URL_ACTUADORES)
+    .then (respuesta => respuesta.json() )
+    .then (resultado => {
+      this.setState({
+        isLoaded3: true,
+        items3: resultado,
+        })
+        console.log(resultado);
+    })
+
   }
 
  /*
@@ -70,13 +100,13 @@ class App extends Component {
       task: ''
     }));
   }
-  */  
+    
   removeTask(id) {
     this.setState(prevState => ({
       tasks: prevState.tasks.filter(el => el.id !== id)
     }));
   }
-
+*/
   componentDidMount() {
     this.pusher = new Pusher(PUSHER_APP_KEY, {
 	  cluster: PUSHER_APP_CLUSTER,
@@ -86,28 +116,49 @@ class App extends Component {
 	
     this.channel.bind('inserted', this.addTask);
     this.channel.bind('deleted', this.addTask);
-       this.channel.bind('updated', this.addTask);
+    this.channel.bind('updated', this.addTask);
 
-      fetch(API_URL)
+      fetch(API_URL_SENSORES)
          .then (respuesta => respuesta.json() )
          .then (resultado => {
            this.setState({
              isLoaded: true,
-             items: resultado,
+             items1: resultado,
            })
+           console.log(resultado);
          })
+
+         fetch(API_URL_UMBRALES)
+         .then (respuesta => respuesta.json() )
+         .then (resultado => {
+           this.setState({
+             isLoaded2: true,
+             items2: resultado,
+             })
+             console.log(resultado);
+         })
+
+         fetch(API_URL_ACTUADORES)
+    .then (respuesta => respuesta.json() )
+    .then (resultado => {
+      this.setState({
+        isLoaded3: true,
+        items3: resultado,
+        })
+        console.log(resultado);
+    })
 
   }
   
    
   render() {
   
-    let tasks = this.state.tasks.map(item =>
-    <Task key={item._id} task={item} onTaskClick={this.deleteTask} />
-    );
+   // let tasks = this.state.tasks.map(item =>
+   // <Task key={item._id} task={item} onTaskClick={this.deleteTask} />
+   // );
     
-    var { isLoaded, items} = this.state;
-    if (!isLoaded){
+    var { isLoaded, isLoaded2, isLoaded3, items1, items2, items3} = this.state;
+    if (!isLoaded || !isLoaded2 || !isLoaded3){
       return <div>Cargando...</div>
     }
     
@@ -118,24 +169,38 @@ class App extends Component {
         <form>
           <h4>Sensores</h4>
         <ul>
-          {items.map(item=>( 
+          {items1.map(item=>( 
             <li key={item._id}>
               {item.tipo} {item.subtipo} {item.medicion}              
             </li>
           ))}
         </ul>
-          <input type="text" className="input-todo" placeholder="New task" onChange={this.updateText} value={this.state.task} />
-          <div className="btn btn-add" onClick={this.postTask}>+</div>
+            <h4>Configuracion Deseada</h4>
+            <ul>
+          {items2.map(item=>( 
+            <li key={item._id}>
+              {item.identificador} {item.descripcion} {item.valor}              
+            </li>
+          ))}
+        </ul>
+        <h4>Status Actuadores</h4>
+            <ul>
+          {items3.map(item=>( 
+            <li key={item._id}>
+              {item.tipo} {item.subtipo} {item.operacion}              
+            </li>
+          ))}
+        </ul>
+
+
         </form>
         
-        <ul>
-          {tasks}
-        </ul>
       </div>
     );
   }
 }
 
+/*
 class Task extends Component {
   constructor(props) {
     super(props);
@@ -154,4 +219,5 @@ class Task extends Component {
   }
 }
 
+*/
 export default App;
