3db3e358a89c8e98f38125fca498e62ad6a2a761
Gaston Raul Valdes <gastonrvaldes@hotmail.com>
2020-07-04 00:07:11 -0300

    mejore la interfaz grafica usando routers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Pusher from 'pusher-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import ReactSelect from './ReactSelect';
 
 const API_URL_SENSORES = 'http://localhost:3010/sensores';
 const API_URL_UMBRALES = 'http://localhost:3010/umbrales';
@@ -47,7 +54,7 @@ console.log(type_ilum.valor);
 
 /***************************************************************************************** */
  //aca va el fetch al registro de temp
- alert (type_temp.valor);
+ //alert (type_temp.valor);
  fetch(API_URL_UMBRALES, {
   method: 'PATCH', //Hago un Patch para actualizar el umbral de temperatura
   headers : { 
@@ -70,7 +77,7 @@ console.log(type_ilum.valor);
                                 }
               );
 //aca va el fetch al registro de ilum
-alert (type_ilum.valor);
+//alert (type_ilum.valor);
 fetch(API_URL_UMBRALES, {
  method: 'PATCH', //Hago un Patch para actualizar el umbral de temperatura
  headers : { 
@@ -108,10 +115,6 @@ fetch(API_URL_CALC, {
 
 }
 //---------------------------------
-
-
-
-
 
   addTask(newTask) {
 //Cargo valores de sensores
@@ -205,61 +208,87 @@ fetch(API_URL_CALC, {
 
   //  alert ('actualizo')
   }
+
+/**************************************************************/
+/**********     EMPIEZO EL RENDER DE PANTALLA     *************/
+
   render() {
   
    
-    
+// Si no se cargaron los datos de los llamados get a las API muestro un mensaje ...cargando
     var { isLoaded, isLoaded2, isLoaded3, items1, items2, items3} = this.state;
     if (!isLoaded || !isLoaded2 || !isLoaded3){
       return <div>Cargando...</div>
     }
-    
-    return (
-      
-     <div className="todo-wrapper">
-      
-        <form>
-        
-        <img src={require('./img/header2.jpg')} alt="imagen Encabezado" with="200" height="100" />
-         
-          <h4>Sensores</h4>
-          {items1.map(item=>( 
-              <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center">
-                {item.tipo} {item.subtipo}  <span className="badge badge-info badge-pill">{item.medicion}</span> 
-              </li>
-           
-          ))}
-          <h4>Configuracion Deseada</h4>
-          {items2.map(item=>( 
 
-          <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center"> {item.descripcion} 
+// Si ya estan cargados los datos de la API empiezo el render    
+    return (
+      <div className="todo-wrapper"> 
+        <div className="row">
+          <img src={require('./img/smart7.jpg')} alt="imagen Encabezado"  with="500" height="170" className="card-img-top"/>
+        </div>
+        <Router>
+          <div className="btn-group">
+            <Link to="/" className="btn btn-primary">
+              Sensores
+            </Link>
+            <Link to="/Configuracion" className="btn btn-primary">
+              Config
+            </Link>
+            <Link to="/Actuadores" className="btn btn-primary">
+              Actuadores
+            </Link>
+            <Link to="/ReactSelect" className="btn btn-primary">
+              Reportes
+            </Link>
+          </div>
+        <Switch>
+          <Route path="/" exact>
+            <form>
+              <h4>Sensores</h4>
+                {items1.map(item=>( 
+                  <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center">
+                    {item.tipo} {item.subtipo}  <span className="badge badge-info badge-pill">{item.medicion}</span> 
+                  </li>))
+                }
+            </form>
+          </Route>
+          <Route path="/Configuracion" exact>
+            <form>
+              <h4>Configuracion Deseada</h4>
+                {items2.map(item=>( 
+                  <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center"> {item.descripcion} 
                   <label htmlform={item.descripcion}></label>
                   <input name={item.descripcion} type="number" min = "10" max = "5000" placeholder={item.valor} onChange={this.handleChange}/>
                   <span className="validity"></span>
                     <div className="custom-control custom-switch">
                       <input type="checkbox" className="custom-control-input" id={item._id} defaultChecked={item.habilitado}/>
                       <label className="custom-control-label" htmlFor={item._id}></label>
-                  </div>
-              </li>
-
-          ))}
-          <button type="button" className="btn btn-primary" onClick={this.grabar}>Grabar</button>
-         <h4>Status Actuadores</h4>
-          {items3.map(item=>( 
-              <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center">
+                    </div>
+                  </li>))
+                }
+                <button type="button" className="btn btn-primary" onClick={this.grabar}>Grabar</button>
+            </form>
+          </Route>
+          <Route path="/Actuadores">
+            <form>
+               <h4>Status Actuadores</h4>
+                {items3.map(item=>( 
+                  <li  key={item._id} className="list-group-item d-flex justify-content-between align-items-center">
                   {item.tipo} {item.subtipo} <span className="badge badge-success badge-pill"> {item.operacion} </span>             
-              </li>
-          ))}
-       
-     
-        
-        </form>
-        
+                  </li>))
+                }
+            </form>
+          </Route>
+          <Route path="/ReactSelect">
+            <ReactSelect/>
+          </Route>
 
-       
-        
+          
+       </Switch>
+     </Router>
+     
       </div>
-         
     );
   }
 }
---
+++ src/ReactSelect.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ReactSelect extends Component {
+  
+  render() {
+      return ( 
+     
+
+     <img src={require('./img/Historial.PNG')} alt="Historial" with="350" height="380" />
+     
+     )
+  }
+
+}
+
+export default ReactSelect;
